fb2239cb69ad661e2f5aef45b60ada9c9be678fd
Jordan Layton <jordan.layton@hotmail.com>
2022-04-06 16:45:56 +0100

    test upload

--- testgym.py
+++ testgym.py
@@ -2,9 +2,11 @@ import gym
 # import pygame
 import distutils.spawn
 
-env = gym.make('CartPole-v1')
+# env = gym.make("Pendulum-v1")
+env = gym.make("BipedalWalker-v3")
 env.reset()
 for _ in range(1000):
     env.render()
     env.step(env.action_space.sample()) # take a random action
+# env.reset()
 env.close()
